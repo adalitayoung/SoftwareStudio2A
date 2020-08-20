9456391fcc081e4481a4f6bd33e595c99c5ffbd6
Gabrielle Walker <13196732@student.uts.edu.au>
2020-08-20 18:55:37 +1000

    psudeo code added to algorithm-ctrl.js

--- AllocationApp/server/controllers/algorithm-ctrl.js
+++ AllocationApp/server/controllers/algorithm-ctrl.js
@@ -8,3 +8,26 @@ startAlgorithm = (req, res) => {
 module.exports = {
     startAlgorithm,
 }
+/* 
+get all student details (preferences and tech expertise)
+get number of students
+get all project quotas
+split students based on tech expertise
+if first preference project quota is not full 
+    allocate students with tech expertise "A" to first preference - add project_ID to temp-student DB 
+    allocate students with tech expertise "b" to first preference - add project_ID to temp-student DB
+    allocate students with tech expertise "c" to first preference - add project_ID to temp-student DB 
+else if
+    allocate students with tech expertise "A" to second preference - add project_ID to temp-student DB
+    allocate students with tech expertise "c" to second preference - add project_ID to temp-student DB
+    allocate students with tech expertise "b" to second preference - add project_ID to temp-student DB
+else if
+    allocate students with tech expertise "A" to third preference - add project_ID to temp-student DB
+    allocate students with tech expertise "b" to third preference - add project_ID to temp-student DB
+    allocate students with tech expertise "c" to third preference - add project_ID to temp-student DB
+else
+    randomly assign to projects with empty spaces 
+    OR
+    Admin can manually assign students to groups with empty spaces 
+    
+*/
