2a8acde1d7225aa78bd1dfb4cbaecc0e68fe4d4d
Gabrielle Walker <13196732@student.uts.edu.au>
2020-08-25 19:04:18 +1000

    fixed comments

--- AllocationApp/server/controllers/algorithm-ctrl.js
+++ AllocationApp/server/controllers/algorithm-ctrl.js
@@ -8,25 +8,27 @@ startAlgorithm = (req, res) => {
 module.exports = {
     startAlgorithm,
 }
+
 /* 
 get all student details (preferences and tech expertise)
 get number of students
-get all project quotas
-split students based on tech expertise
+get all project quotas - group size background requirements
+split students based on their background
 if first preference project quota is not full 
     allocate students with tech expertise "A" to first preference - add project_ID to temp-student DB 
     allocate students with tech expertise "b" to first preference - add project_ID to temp-student DB
     allocate students with tech expertise "c" to first preference - add project_ID to temp-student DB 
-else if
+else if second preference project quota is not full 
     allocate students with tech expertise "A" to second preference - add project_ID to temp-student DB
     allocate students with tech expertise "c" to second preference - add project_ID to temp-student DB
     allocate students with tech expertise "b" to second preference - add project_ID to temp-student DB
-else if
+else if third preference project quota is not full 
     allocate students with tech expertise "A" to third preference - add project_ID to temp-student DB
     allocate students with tech expertise "b" to third preference - add project_ID to temp-student DB
     allocate students with tech expertise "c" to third preference - add project_ID to temp-student DB
 else
     randomly assign to projects with empty spaces 
-    OR
-    Admin can manually assign students to groups with empty spaces 
+   
 */
+
+// Admin can manually assign left over students to groups with empty spaces 
