c5f16584bc906fc6609143595983c4724d3a7805
Rachana Bhat <13274030@student.uts.edu.au>
2020-08-27 21:32:54 +1000

    new updated homepage

--- AllocationApp/client/src/components/Home.jsx
+++ AllocationApp/client/src/components/Home.jsx
@@ -1,45 +1,55 @@
 import React from "react";
-import grad from './grad.png';
-import background from './background.jpg';
 
-console.log(grad);
+import background from './updatedbackground.jpg';
+import { Link } from 'react-router-dom'
+
+
 console.log(background);
 
 function Home() {
-  const mystyle = {
-    color: "white",
-    backgroundColor: "DodgerBlue",
-    padding: "20px",
-    fontFamily: "Arial",
-    margin: "6px 2px",
-    marginLeft: "30px",
-    fontSize: "16px",
-    borderRadius: "12px"
-  };
+    const hpbutton = {
+        color: "black",
+        backgroundColor: "LightGrey",
+        padding: "10px 24px",
+        fontFamily: "Arial",
+        margin: "6px 4px",
+        textAlign: "center",
+        position: "absoluteright",
+        
+        fontSize: "20px",
+        borderRadius: "12px",
+        display: "inline-block",
+} 
   return (
       <div className="home" >
           <div class="container" >
-        
-                      <div className="box" >
+              < div class="piccontainer">
+                  <img className="hppic" src={background} width="100%" />
+                  <div class= "text-block">
+              <div className="box" >
+                 
             <h1 class="font-weight-light"></h1>
-              <img src={require("./grad.png")} alt="grad" width="200" height="150"/>
+             
               
 
-              <h1>GRADONLINE</h1>
+              <div className="fontgradon">GRADONLINE</div>
               <h2>Automatic Student Group Allocation</h2>
                           <br></br>
               </div>
               <div className="box">
           
-              
-              <button type="button" style={mystyle}>SIGN IN</button>
-              
-              <button type="button" style={mystyle}>SIGN OUT</button>
-              
+              <Link to="Signin">
+              <button type="button" style={hpbutton}>SIGN IN</button>
+                  </Link>
+                <Link to="Register">
+              <button type="button" style={hpbutton}>REGISTER</button>
+              </Link>
               </div>
          
       </div>
-    </div>
+     </div>
+          </div>
+          </div>
   );
 }
 
