bea7ad20caae4f11667805a4d2b9d41cae359ed9
Wai30Yan <42363348+Wai30Yan@users.noreply.github.com>
2020-08-27 17:45:23 +1000

    unfinished SignIn

--- AllocationApp/client/src/components/Signin.jsx
+++ AllocationApp/client/src/components/Signin.jsx
@@ -1,20 +1,65 @@
-import React from "react";
+import React, { Component } from "react";
+import '../style/style.css';
+import api from '../api'
+import name from '../name.png'
+import lock from '../lock.png'
 
-function Signin() {
-  return (
-    <div className="signin">
-      <div class="container">
-        <div class="row align-items-center">
-          <div class="col-lg-5">
-            <h1 class="font-weight-light">Signin</h1>
-            <p>
-              WAI YAN
-            </p>
+import { Link } from 'react-router-dom'
+
+class Signin extends Component {
+
+  constructor(props){
+    super(props)
+    this.state = {
+      fullName: '',
+      password: ''
+    }
+  }
+
+  handleChangeName = async event => {
+    const fullName = event.target.value;
+    this.setState({fullName})
+  }
+
+  handleChangePassword = async event => {
+    const password = event.target.value;
+    this.setState({password})
+  }
+
+  handleSignin = async event => {
+    event.preventDefault();
+    const {fullName, password} = this.state
+    if (fullName !== '' && password !== '') {
+      
+    }
+  }
+
+  render(){
+    return (
+      <div className="signin">
+        <div className="container">
+          <div className="row align-items-center">
+            <div className="col-lg-5">
+              <h1 className="font-weight-light">Signin</h1>
+              <form>
+                <div className="form-group">
+                  <label htmlFor="exampleInputName"><img className="name" src={name} width="15px"/>Full Name</label>
+                  <input type="name" className="form-control" id="exampleInputName" onChange={this.handleChangeName} placeholder="Enter Full Name"></input>
+                </div>
+                <div className="form-group">
+                  <label htmlFor="exampleInputPassword1"><img className="lock" src={lock} width="15px"/> Password</label>
+                  <input type="password" className="form-control" id="exampleInputPassword1" onChange={this.handleChangePassword} placeholder="Password"></input>
+                </div>
+                <button id="sign" className="btn btn-primary btn-block" onClick={this.handleSignin}>Sign in</button>
+                <p className="account"><Link to="" className="nav-link">Forgot your password?</Link></p>
+              </form>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
+  
 }
 
 export default Signin;
