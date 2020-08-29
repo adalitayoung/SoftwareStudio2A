4f502f090a970c1b194a41900421c363b475d327
Lucky Jaiswal <jaiswal.lucky8888@gmail.com>
2020-08-29 23:48:57 +1000

    updated project-router with create/ show project controllers

--- AllocationApp/server/routes/project-router.js
+++ AllocationApp/server/routes/project-router.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const ProjectRolesCtrl = require('../controllers/projectRoles-ctrl')
+const ProjectCtrl = require('../controllers/project-ctrl')
 const router = express.Router();
 
 router.get('/showRoles', ProjectRolesCtrl.showProjectRoles)
 router.post('/createProjectRole', ProjectRolesCtrl.createProjectRoles)
+
+router.get('/showProject', ProjectCtrl.showProject)
+router.post('/createProject', ProjectCtrl.createProject)
 
 module.exports = router
