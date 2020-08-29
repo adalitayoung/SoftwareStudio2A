5a7e8420545acfe8d9fb2b2df03eba5d63b8b5ce
Lucky Jaiswal <jaiswal.lucky8888@gmail.com>
2020-08-29 15:59:28 +1000

    Fixed a typo

--- AllocationApp/server/controllers/project-ctrl.js
+++ AllocationApp/server/controllers/project-ctrl.js
@@ -4,7 +4,7 @@ createProject = (req, res) => {
   const body = req.body
   if (!body) {
     return res.status(400).json({
-      succes: false,
+      success: false,
       error: 'You must provide a project'
     })
   }
