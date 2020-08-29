e505a7f5add7599f2e6c618a7c14dedb12e02753
Lucky Jaiswal <jaiswal.lucky8888@gmail.com>
2020-08-29 23:38:01 +1000

    Updated project controller to with save/show projects from mongoDB

--- AllocationApp/server/controllers/project-ctrl.js
+++ AllocationApp/server/controllers/project-ctrl.js
@@ -1,5 +1,12 @@
 const Project = require('../models/project-model.js')
 
+showProject = (req, res) => {
+  Project.find()
+  .then(projects => res.json(projects))
+  .catch(err => res.status(400).json('Error: ' + err))
+}
+
+
 createProject = (req, res) => {
   const body = req.body
   if (!body) {
@@ -8,8 +15,31 @@ createProject = (req, res) => {
       error: 'You must provide a project'
     })
   }
+  const classID = "" // this will come from classID
+  const createdByID = "TeacherID" // this is to be changed to project creator's ID
+  const createdByname = "TeacherName"
+  const projectName = req.body.projectName
+  const description = req.body.description
+  const studentMin = req.body.studentMin
+  const studentMax = req.body.studentMax
 
-  const project = new Project(body)
+  const newProject = new Project({
+    classID,
+    createdByID,
+    createdByname,
+    projectName,
+    description,
+    studentMin,
+    studentMax
+  })
 
-  if (!project)
-}
+  newProject.save()
+    .then(() => res.json("Project Created"))
+    .catch(err => res.status(400).json('Error: ' + err))
+
+ }
+
+ module.exports = {
+   showProject,
+   createProject
+ }
