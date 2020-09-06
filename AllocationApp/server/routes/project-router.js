04ed4ab34912e1b3f9d2d7bf43e20bab34aeaac3
Lucky Jaiswal <jaiswal.lucky8888@gmail.com>
2020-09-07 04:21:12 +1000

    Added verifytoken function

--- AllocationApp/server/routes/project-router.js
+++ AllocationApp/server/routes/project-router.js
@@ -2,12 +2,13 @@ const express = require('express')
 const ProjectRolesCtrl = require('../controllers/projectRoles-ctrl')
 const ProjectCtrl = require('../controllers/project-ctrl')
 const router = express.Router();
+const verify = require('../verifyTokens')
 
 router.get('/showRoles', ProjectRolesCtrl.showProjectRoles)
 router.post('/createProjectRole', ProjectRolesCtrl.createProjectRoles)
 
 router.get('/showProject/:id', ProjectCtrl.showProject)
-router.post('/createProject', ProjectCtrl.createProject)
+router.post('/createProject',  ProjectCtrl.createProject)
 router.post('/updateProject/:id',ProjectCtrl.updateProject)
 router.delete('/:id',ProjectCtrl.deleteProject)
 
