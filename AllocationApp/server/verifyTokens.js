e428b4dace1abeb7e974dbccc7e506b4f566de78
Lucky Jaiswal <jaiswal.lucky8888@gmail.com>
2020-09-08 18:14:27 +1000

    Updated VerifyToken code to include userinfo in request sothat this can be used in authentication of logged in user

--- AllocationApp/server/verifyTokens.js
+++ AllocationApp/server/verifyTokens.js
@@ -1,16 +1,24 @@
 const jwt = require('jsonwebtoken')
+const User = require('./models/user-model.js')
 
 module.exports = function(req, res, next) {
   const token = req.header('auth-token')
   if(!token) return res.status(401).send('Access Denied')
-
-  try{
     const verified = jwt.verify(token, process.env.TOKEN_CODE)
-    req.users = verified
+    //req.users = verified
+    User.find({_id:verified._id}).exec(function(err, userinfo){
+        if (!userinfo.length){
+            return res.status(400).json({
+                success: false,
+                error: 'User not found'
+            })
+        }else{
+          req.userName=userinfo[0].fullName
+          req.userID=userinfo[0]._id
+          req.userRole=userinfo[0].role
+          //console.log(userinfo)
+        }
     next()
-    console.log(verified)
-  } catch(err){
-    res.status(400).send('Invalid Token')
-  }
+  })
 
 }
