76680f4da4d84b2c124d9a26c8c6cf97a4dafa3a
Gabrielle Walker <13196732@student.uts.edu.au>
2020-09-24 15:19:09 +1000

    update class details complete

--- AllocationApp/server/routes/class-router.js
+++ AllocationApp/server/routes/class-router.js
@@ -8,5 +8,6 @@ router.post('/createClass', ClassCtrl.createCourse);
 router.get('/getClass/:name', ClassCtrl.returnCourseByName);
 router.get('/getAllClasses', ClassCtrl.returnAllCourses);
 router.delete('/deleteClass/:id', ClassCtrl.deleteCourse);
+router.post('/updateClass/:id/:name/:numberOfStudents', ClassCtrl.updateCourse);
 
 module.exports = router
